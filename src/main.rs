20d408a34c7412e6ab8abdd85bfbeb51b67fda96
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-05-04 15:32:46 +0200

    feat: add clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use clap::{Parser, Subcommand};
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let args = Args::parse();
+}
+
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Turn debugging information on
+    #[clap(short, long, parse(from_occurrences))]
+    debug: usize,
+
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[clap(short, long)]
+        list: bool,
+    },
 }
